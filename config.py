23b0f425d1756004779f016b132afa52e777e708
Nsanga-lab <sangan896@gmail.com>
2026-08-29 13:36:17 +0300

    Add data collection scripts

---
+++ config.py
@@ -0,0 +1,18 @@
+"""
+Configuration file for FOSS Research Project
+"""
+
+# Google Sheets configuration
+GOOGLE_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1iDa0Psp2vtgOCmBKn8VFc4BG1KqH9U8702rEolqUquQ/edit'
+SHEET_ID = '1iDa0Psp2vtgOCmBKn8VFc4BG1KqH9U8702rEolqUquQ'
+
+# Data directories
+RAW_DATA_DIR = 'data/raw'
+PROCESSED_DATA_DIR = 'data/processed'
+
+# File naming
+SURVEY_FILENAME = 'survey_responses'
+LATEST_FILENAME = 'survey_responses_latest.csv'
+
+# Analysis settings
+SEED = 42  # For reproducibility
